5c13493be02052787979350f2043983f08c64a9e
Stephen Salerno <stps@users.noreply.github.com>
2024-07-03 22:12:29 -0500

    fix AVFoundation needing macOS-only CoreAudio APIs

--- audio/out/ao_coreaudio_chmap.h
+++ audio/out/ao_coreaudio_chmap.h
@@ -26,10 +26,10 @@ struct mp_chmap;
 int ca_label_to_mp_speaker_id(AudioChannelLabel label);
 AudioChannelLabel mp_speaker_id_to_ca_label(int speaker_id);
 
-#if HAVE_COREAUDIO || HAVE_AVFOUNDATION
 AudioChannelLayout *ca_find_standard_layout(void *talloc_ctx, AudioChannelLayout *l);
 AudioChannelLayout *ca_get_acl(struct ao *ao, size_t *out_layout_size);
 void ca_log_layout(struct ao *ao, int l, AudioChannelLayout *layout);
+#if HAVE_COREAUDIO
 bool ca_init_chmap(struct ao *ao, AudioDeviceID device);
 void ca_get_active_chmap(struct ao *ao, AudioDeviceID device, int channel_count,
                          struct mp_chmap *out_map);
